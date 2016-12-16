6b115e41df951e6c1103c93c9bb61e203eafb856
isaacahuvia <isaacahuvia@gmail.com>
2016-12-16 17:35:16 -0600

    Add files via upload

--- App.R
+++ App.R
@@ -1,9 +1,6 @@
 ##########################
 ## Ping Pong Shiny App  ##
-##     Hover Branch     ##
 ##########################
-# 
-# setwd("C:\\Users\\ahuvia\\Desktop\\Really Important Stuff\\Ping Pong - Hover Branch")
 
 ####  Startup  ####
 rm(list = ls())
