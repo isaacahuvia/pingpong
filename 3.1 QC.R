5d8d0df3939fd6582ab5958b94248b9f8cbd2455
Isaac Ahuvia <isaacahuvia@gmail.com>
2017-02-21 18:42:16 -0600

    Delete 3.1 QC.R

--- 3.1 QC.R
+++
@@ -1,7 +0,0 @@
-##########
-##  QC  ##
-##########
-
-dfMatch$QCFlag <- FALSE
-dfMatch$QCFlag[dfMatch$IsaacFinalScore != dfMatch$IsaacFinalScoreRecorded] <- TRUE
-dfMatch$QCFlag[dfMatch$TimiFinalScore != dfMatch$TimiFinalScoreRecorded] <- TRUE
